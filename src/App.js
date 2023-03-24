d66f62b7b63c895045874f3032a0b1fe25d442ff
shiyu.czm <shiyu.czm@alibaba-inc.com>
2023-03-24 11:15:44 +0800

    add submodule

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Counter />
-      </header>
-    </div>
-  );
+import * as React from 'react';
+class App extends React.Component {
+  state = {
+    list: ['A', 'B', 'C'],
+  };
+  onChange = () => {
+    this.setState({ list: ['C', 'A', 'X'] });
+  };
+  componentDidMount() {
+    console.log(`App Mount`);
+  }
+  render() {
+    return (
+      <>
+        <Header />
+        <button onClick={this.onChange}>change</button>
+        <div className="content">
+          {this.state.list.map((item) => (
+            <p key={item}>{item}</p>
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>{count}</p>
-      <button onClick={() => setCount((count) => count + 1)}>add</button>
-    </div>
-  );
+class Header extends React.PureComponent {
+  render() {
+    return (
+      <>
+        <h1>title</h1>
+        <h2>title2</h2>
+      </>
+    );
+  }
 }
-
 export default App;
+
